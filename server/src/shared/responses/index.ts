185210d8a909f542562c04952eb02eeb5f2d4763
Damola Adegbite <damolaadegbite0@gmail.com>
2026-04-17 23:26:48 +0100

    refactor: remove file extensions from response exports

--- server/src/shared/responses/index.ts
+++ server/src/shared/responses/index.ts
@@ -1,1 +1,3 @@
-export { sendSuccess } from './sendSuccess.js';
+export { sendSuccess } from './sendSuccess';
+export { sendError } from './sendError';
+export type { ErrorEnvelope, ErrorPayload, ResponseMessage, SuccessEnvelope } from './types';
