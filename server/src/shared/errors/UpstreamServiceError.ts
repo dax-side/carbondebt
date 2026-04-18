7057d9f232dd5ff943fab21a6bd734930d20e01a
Damola Adegbite <damolaadegbite0@gmail.com>
2026-04-18 16:17:29 +0100

    refactor: allow customizable status code in UpstreamServiceError constructor

--- server/src/shared/errors/UpstreamServiceError.ts
+++ server/src/shared/errors/UpstreamServiceError.ts
@@ -1,8 +1,8 @@
 import { AppError } from './AppError';
 
 export class UpstreamServiceError extends AppError {
-  constructor(message: string = 'Upstream service error', details?: unknown) {
-    super(message, 502, true, details);
+  constructor(message: string = 'Upstream service error', details?: unknown, statusCode = 502) {
+    super(message, statusCode, true, details);
     Object.setPrototypeOf(this, new.target.prototype);
   }
 }
